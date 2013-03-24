09fd4e65bf1f28240addf2b830036bf48b8fe8d1
Olavi Lintumäki <atomimalli@gmail.com>
2013-03-24 23:13:52 +0200

    move dependency injection to spring

--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kirjanpito.java
@@ -2,7 +2,7 @@
 package ohtu.verkkokauppa;
 
 import java.util.ArrayList;
-
+import org.springframework.stereotype.Component;
 public class Kirjanpito implements IKirjanpito {
 
     private ArrayList<String> tapahtumat;
--- viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -1,12 +1,13 @@
 package ohtu.verkkokauppa;
 
 import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 public class Varasto implements IVarasto {
 
     private IKirjanpito kirjanpito;
     private HashMap<Tuote, Integer> saldot;
-
     public Varasto(IKirjanpito kirjanpito) {
         this.kirjanpito = kirjanpito;
         saldot = new HashMap<Tuote, Integer>();
