cc01e29ba2026e7b0fd35fdb57d61acb60da66c4
Olavi Lintumäki <atomimalli@gmail.com>
2013-04-14 18:06:44 +0300

    v4t2

--- viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
+++ viikko4/LoginWeb2/src/main/java/ohtu/Tester.java
@@ -10,6 +10,7 @@ public class Tester {
     public static void main(String[] args) {
         WebDriver driver = new HtmlUnitDriver();
 
+        //onnistunut kirjautuminen
         driver.get("http://localhost:8080");
         System.out.println( driver.getPageSource() );
         WebElement element = driver.findElement(By.linkText("login"));       
@@ -28,5 +29,82 @@ public class Tester {
         System.out.println("==");
         System.out.println( driver.getPageSource() );
         
+        //väärä salasana
+        driver.get("http://localhost:8080");
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.linkText("login"));       
+        element.click(); 
+        
+        System.out.println("==");
+        
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkepäää");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+        
+        System.out.println("==");
+        System.out.println( driver.getPageSource() );
+        
+        //väärä tunnus
+        driver.get("http://localhost:8080");
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.linkText("login"));       
+        element.click(); 
+        
+        System.out.println("==");
+        
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekkaäää");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+        
+        System.out.println("==");
+        System.out.println( driver.getPageSource() );
+        
+        //uusi tunnus
+        driver.get("http://localhost:8080");
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.linkText("register new user"));       
+        element.click(); 
+        
+        System.out.println("==");
+        
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka2");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep2");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("akkep2");
+        element = driver.findElement(By.name("add"));
+        element.submit();
+        
+        System.out.println("==");
+        System.out.println( driver.getPageSource() );
+        
+        //kirjautuminen uudella
+        driver.get("http://localhost:8080");
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.linkText("login"));       
+        element.click(); 
+        
+        System.out.println("==");
+        
+        System.out.println( driver.getPageSource() );
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka2");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("akkep2");
+        element = driver.findElement(By.name("login"));
+        element.submit();
+        
+        System.out.println("==");
+        System.out.println( driver.getPageSource() );
     }
 }
