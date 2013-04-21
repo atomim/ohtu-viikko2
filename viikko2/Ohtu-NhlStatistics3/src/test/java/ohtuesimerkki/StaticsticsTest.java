7f3dfca36536a56987e666bb4c0d156fdef37df7
Olavi Lintumäki <atomimalli@gmail.com>
2013-04-22 02:32:50 +0300

    v5t12

--- viikko2/Ohtu-NhlStatistics3/src/test/java/ohtuesimerkki/StaticsticsTest.java
+++ viikko2/Ohtu-NhlStatistics3/src/test/java/ohtuesimerkki/StaticsticsTest.java
@@ -4,27 +4,42 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class StaticsticsTest {
 
     Statistics stats;
-    PlayerReader palayerReader = new PlayerReader() {
+    PlayerReader palayerReader; /*new PlayerReader() {
 
-        public List<Player> getPlayers() {
-            ArrayList<Player> players = new ArrayList<Player>();
+     public List<Player> getPlayers() {
+     ArrayList<Player> players = new ArrayList<Player>();
 
-            players.add(new Player("Semenko", "EDM", 4, 12));
-            players.add(new Player("Lemieux", "PIT", 45, 54));
-            players.add(new Player("Kurri", "EDM", 37, 53));
-            players.add(new Player("Yzerman", "DET", 42, 56));
-            players.add(new Player("Gretzky", "EDM", 35, 89));
+     players.add(new Player("Semenko", "EDM", 4, 12));
+     players.add(new Player("Lemieux", "PIT", 45, 54));
+     players.add(new Player("Kurri", "EDM", 37, 53));
+     players.add(new Player("Yzerman", "DET", 42, 56));
+     players.add(new Player("Gretzky", "EDM", 35, 89));
 
-            return players;
-        }
-    };
+     return players;
+     }
+     };
+     */
+
 
     @Before
     public void setUp() {
+        palayerReader = mock(PlayerReader.class);
+        
+
+        ArrayList<Player> players = new ArrayList<Player>();
+
+        players.add(new Player("Semenko", "EDM", 4, 12));
+        players.add(new Player("Lemieux", "PIT", 45, 54));
+        players.add(new Player("Kurri", "EDM", 37, 53));
+        players.add(new Player("Yzerman", "DET", 42, 56));
+        players.add(new Player("Gretzky", "EDM", 35, 89));
+
+        when(palayerReader.getPlayers()).thenReturn(players);
         stats = new Statistics(palayerReader);
     }
 
@@ -37,18 +52,18 @@ public class StaticsticsTest {
         assertEquals(45 + 54, p.getPoints());
 
     }
-    
+
     @Test
-    public void teamMembersFound(){
+    public void teamMembersFound() {
         List<Player> players = stats.team("EDM");
         assertEquals(3, players.size());
         for (Player player : players) {
             assertEquals("EDM", player.getTeam());
         }
     }
-    
+
     @Test
-    public void topScorersFound(){
+    public void topScorersFound() {
         List<Player> players = stats.topScorers(2);
         assertEquals(3, players.size());
         assertEquals("Gretzky", players.get(0).getName());
